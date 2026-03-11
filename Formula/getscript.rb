00bda161282221087e1115c0461c51bd51a7c345
Vikram Khare <vikram.khare@gmail.com>
2026-03-11 00:20:17 +0000

    Add getscript formula v0.11.0

---
+++ Formula/getscript.rb
@@ -0,0 +1,52 @@
+class Getscript < Formula
+  include Language::Python::Virtualenv
+
+  desc "Fast, Unix-friendly CLI for fetching transcripts from YouTube and Apple Podcasts"
+  homepage "https://github.com/outerbanks73/cli-tools"
+  url "https://github.com/outerbanks73/cli-tools/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "828403883f2620c3ab97d0e0c71da6b0d16029bdcbdd07064f3a6a820158e0f1"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  resource "youtube-transcript-api" do
+    url "https://files.pythonhosted.org/packages/f2/0a/01940a9e06a53f55a6e5a3cb010c09e9e9ace9f7e3d86449cc53eb1b86d2/youtube_transcript_api-1.0.3.tar.gz"
+    sha256 "8d020104c60ec67a1ddfbdfc42b8af7e7e3e5acb34ed1a62e657a1daa3be11b4"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.3.tar.gz"
+    sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/16/b0/572805e227f01586461c80e0fd25d65a2115599cc9dad142fee4b747c357e/charset_normalizer-3.4.1.tar.gz"
+    sha256 "44251f18cd68a75b56585dd00dae26183e102cd5e0f9f1466e6df5da2ed64ea3"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/f1/70/7703c29685631f5a7590aa73f1f1d3fa9a380e654b86af429e0934a32f7d/idna-3.10.tar.gz"
+    sha256 "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/aa/63/e53da845320b757bf29ef6a9062f5c669fe997973f966045cb019c3f4b66/urllib3-2.3.0.tar.gz"
+    sha256 "f8c5449b3cf0861679ce7e0503c7b44b5ec981bec0d1d3795a07f1ba96f0c1b5"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/1c/ab/c9f1e32b7b1bf505bf26f0ef697775960db7932abeb7b516de930ba2705f/certifi-2025.1.31.tar.gz"
+    sha256 "3d5da6925056f6f18f119200434a4780a94263f10d1c21d032a6f6b2baa20651"
+  end
+
+  def install
+    virtualenv_install_with_resources
+
+    man1.install "man/getscript.1"
+  end
+
+  test do
+    assert_match "getscript", shell_output("#{bin}/getscript --help")
+    assert_match version.to_s, shell_output("#{bin}/getscript --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,23 @@
+# Homebrew Tap
+
+Homebrew formulae for Voxly CLI tools.
+
+## Install
+
+```bash
+brew tap outerbanks73/tap
+brew install getscript
+```
+
+## Tools
+
+| Formula | Description |
+|---------|-------------|
+| [getscript](Formula/getscript.rb) | Fast, Unix-friendly CLI for fetching transcripts from YouTube and Apple Podcasts |
+
+## Updating
+
+```bash
+brew update
+brew upgrade getscript
+```
