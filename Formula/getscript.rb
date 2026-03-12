eaaaf91b8a2caa10dd5e52dd7a1f12554b429d3e
Vikram Khare <vikram.khare@gmail.com>
2026-03-12 21:57:21 +0000

    Fix resource URLs clobbered by automated update, add defusedxml

--- Formula/getscript.rb
+++ Formula/getscript.rb
@@ -10,33 +10,38 @@ class Getscript < Formula
   depends_on "python@3.12"
 
   resource "youtube-transcript-api" do
-    url "https://files.pythonhosted.org/packages/source/g/getscript/getscript-0.12.0.tar.gz"
-    sha256 "6a69c7566bc00742c548e0d584c58399b8981214f9f10ba304f3a11ac34e9b37"
+    url "https://files.pythonhosted.org/packages/60/43/4104185a2eaa839daa693b30e15c37e7e58795e8e09ec414f22b3db54bec/youtube_transcript_api-1.2.4.tar.gz"
+    sha256 "b72d0e96a335df599d67cee51d49e143cff4f45b84bcafc202ff51291603ddcd"
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/source/g/getscript/getscript-0.12.0.tar.gz"
-    sha256 "6a69c7566bc00742c548e0d584c58399b8981214f9f10ba304f3a11ac34e9b37"
+    url "https://files.pythonhosted.org/packages/c9/74/b3ff8e6c8446842c3f5c837e9c3dfcfe2018ea6ecef224c710c85ef728f4/requests-2.32.5.tar.gz"
+    sha256 "dbba0bac56e100853db0ea71b82b4dfd5fe2bf6d3754a8893c3af500cec7d7cf"
+  end
+
+  resource "defusedxml" do
+    url "https://files.pythonhosted.org/packages/0f/d5/c66da9b79e5bdb124974bfe172b4daf3c984ebd9c2a06e2b8a4dc7331c72/defusedxml-0.7.1.tar.gz"
+    sha256 "1bb3032db185915b62d7c6209c5a8792be6a32ab2fedacc84e01b52c51aa3e69"
   end
 
   resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/source/g/getscript/getscript-0.12.0.tar.gz"
-    sha256 "6a69c7566bc00742c548e0d584c58399b8981214f9f10ba304f3a11ac34e9b37"
+    url "https://files.pythonhosted.org/packages/1d/35/02daf95b9cd686320bb622eb148792655c9412dbb9b67abb5694e5910a24/charset_normalizer-3.4.5.tar.gz"
+    sha256 "95adae7b6c42a6c5b5b559b1a99149f090a57128155daeea91732c8d970d8644"
   end
 
   resource "idna" do
-    url "https://files.pythonhosted.org/packages/source/g/getscript/getscript-0.12.0.tar.gz"
-    sha256 "6a69c7566bc00742c548e0d584c58399b8981214f9f10ba304f3a11ac34e9b37"
+    url "https://files.pythonhosted.org/packages/6f/6d/0703ccc57f3a7233505399edb88de3cbd678da106337b9fcde432b65ed60/idna-3.11.tar.gz"
+    sha256 "795dafcc9c04ed0c1fb032c2aa73654d8e8c5023a7df64a53f39190ada629902"
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/source/g/getscript/getscript-0.12.0.tar.gz"
-    sha256 "6a69c7566bc00742c548e0d584c58399b8981214f9f10ba304f3a11ac34e9b37"
+    url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
+    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
   end
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/source/g/getscript/getscript-0.12.0.tar.gz"
-    sha256 "6a69c7566bc00742c548e0d584c58399b8981214f9f10ba304f3a11ac34e9b37"
+    url "https://files.pythonhosted.org/packages/af/2d/7bf41579a8986e348fa033a31cdd0e4121114f6bce2457e8876010b092dd/certifi-2026.2.25.tar.gz"
+    sha256 "e887ab5cee78ea814d3472169153c2d12cd43b14bd03329a39a9c6e2e80bfba7"
   end
 
   def install
